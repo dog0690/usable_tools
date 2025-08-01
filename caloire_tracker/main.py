0bca143fb08c8a01743009e114dbdf9d897481f4
dog0690 <charleymaivn@gmail.com>
2025-08-01 17:40:59 -0400

    more management

--- caloire_tracker/main.py
+++ caloire_tracker/main.py
@@ -1,27 +1,39 @@
 import random
 import time
-class food():
-    def __init__(self, name: str, protien: str, carbs: str, fats: str ) -> dict [str, int] :
+class my_macro():
+    def __init__(self, name: str, protien: int, carbs: int, fats: int ) -> dict [str, int] :
         self.name = name
         self.protien = protien
+        self.carbs = carbs
+        self.fats = fats
 
-class measurement():
+class food():
     def __init__(self, macro: str, level: str, max_amount: int) -> dict [str, int]:
         self.macro = macro
         self.level = level
         self.max_amount = max_amount
 
-    def High(self) -> int:
-            high = (self.max_amount * random.uniform(20, 25))/100
-            return f"{round(high, 2)}g"
-        
+#    def High(self) -> int:
+#            high = (self.max_amount * random.uniform(20, 25))/100
+#            return f"{round(high, 2)}g"
+    def count(self, level) -> int:
+            if level == "H":
+                high = (self.max_amount * random.uniform(20, 25))/100
+                amount = f"{round(high, 2)}g"
+            elif level == "M":
+                medium = (self.max_amount * random.uniform(14, 19))/100
+                amount=  f"{round(medium, 2)}g"
+            elif level == "L":
+                low = (self.max_amount * random.uniform(9, 13))/100
+                amount=  f"{round(low, 2)}g"
+            else:
+                amount = "please enter H, M, L"
+            return amount
 
-protien = measurement('protine', 'H', 14)
-carbs = measurement('carbs', 'H', 24)
-fats = measurement('fats', 'H', 34)
-
+me = my_macro('charley',124, 250, 34 )
+cookie = food('protien', 'H', me.protien)
 def main():
     while True:
-        print(measurement.High(protien))
+        print(food.count(cookie, cookie.level))
         time.sleep(1)
 main()
