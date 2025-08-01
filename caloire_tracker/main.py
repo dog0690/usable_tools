4ec5f2ead27958b7ad4fe0c9d8fbff9adce01832
dog0690 <charleymaivn@gmail.com>
2025-08-01 17:18:28 -0400

    more revised counter

--- caloire_tracker/main.py
+++ caloire_tracker/main.py
@@ -1,22 +1,27 @@
+import random
+import time
 class food():
     def __init__(self, name: str, protien: str, carbs: str, fats: str ) -> dict [str, int] :
         self.name = name
         self.protien = protien
 
 class measurement():
-    def __init__(self, macro: str, level: str, amount: int) -> dict [str, int]:
+    def __init__(self, macro: str, level: str, max_amount: int) -> dict [str, int]:
         self.macro = macro
         self.level = level
-        self.amount = amount
+        self.max_amount = max_amount
 
     def High(self) -> int:
-        if self.level == 'H':
-            high = self.amount * .2
-            return round(high, 2)
+            high = (self.max_amount * random.uniform(20, 25))/100
+            return f"{round(high, 2)}g"
         
 
 protien = measurement('protine', 'H', 14)
-carbs = measurement('protine', 'H', 24)
-fats = measurement('protine', 'H', 34)
+carbs = measurement('carbs', 'H', 24)
+fats = measurement('fats', 'H', 34)
 
-print(measurement.High(protien))
+def main():
+    while True:
+        print(measurement.High(protien))
+        time.sleep(1)
+main()
